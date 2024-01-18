4fa0a9e7cfa8017a8c47ea1f463c4123f396d5a5
federicoverbicaro <federico.verbicaro@icloud.com>
2024-01-18 16:54:25 +0100

    feat: seconda parte

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,18 @@ const spanHtml = document.getElementById("tempo")
 const oreDiUnGiorno = 24
 const minutiInUnOra = 60 
 const secondiDiUnMinuto = 60
-const millesecondiDiUnSecondo = 1000
+let millesecondiDiUnSecondo = 1000
 
+
+let secondi = 0
+let scandenza = "19/01/2024"
+let oraScadenza = 9.30
+
+
+//time ora reale
+setInterval(myTimer)
+
+function myTimer(tempo){
+    const date = new Date()
+    spanHtml.innerHTML = date.toLocaleTimeString()
+}
